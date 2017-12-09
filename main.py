cebdcabb1874a193d8fdbe3d30abb6e9dece1eea
textstorm <jiangzhewise@gmail.com>
2017-12-09 20:43:00 +0800

    add infer

--- main.py
+++ main.py
@@ -15,22 +15,24 @@ def run_sample_decode(args, infer_model, infer_sess, model_dir, infer_data):
     loaded_infer_model, global_step = model_helper.create_or_load_model(
         infer_model.model, model_dir, infer_sess, "infer")
 
-  _sample_decode(args, loaded_infer_model, global_step, infer_sess, infer_data)
+  _sample_decode(args, loaded_infer_model, global_step, infer_sess, infer_model.iterator, 
+      infer_data, infer_model.data_placeholder, infer_model.batch_size_placeholder)
 
-def _sample_decode(args, model, global_step, sess, infer_data):
-  infer_data = utils.get_batches(infer_data, 1)
+def _sample_decode(args, model, global_step, sess, iterator, infer_data, 
+                   iterator_data_placeholder, iterator_batch_size_placeholder):
   decode_id = random.randint(0, len(infer_data) - 1)
-  decode_input = infer_data[decode_id]
-  sample_id, sample_words = model.infer(sess, decode_input.user, 
-      decode_input.product, decode_input.rating, decode_input.review_length)
+  iterator_feed_dict = {
+      iterator_data_placeholder: [infer_data[decode_id]],
+      iterator_batch_size_placeholder: 1,
+  }
+  sess.run(iterator.initializer, feed_dict=iterator_feed_dict)
+  sample_id, sample_words = model.infer(sess)
 
   if args.beam_width > 0:
     sample_words = sample_words[0]
 
-  utils.print_out("user_id: %d" % decode_input.user[0])
-  utils.print_out("product_id: %d" % decode_input.product[0])
-  utils.print_out("rating: %d" % decode_input.rating[0])
-  utils.print_out("gen_review: %s" % (" ".join(sample_words)))
+  utils.print_out("src sentence: %s" % infer_data[decode_id])
+  utils.print_out("gen sentence: %s" % (" ".join(sample_words)))
 
 def run_internal_eval(args, eval_model, eval_sess, model_dir, eval_data):
   with eval_model.graph.as_default():
@@ -60,14 +62,14 @@ def main(args):
 
   train_model = model_helper.build_train_model(args, use_attention=False)
   eval_model = model_helper.build_eval_model(args, use_attention=False)
-  #infer_model = model_helper.build_infer_model(args, use_attention=False)
+  infer_model = model_helper.build_infer_model(args, use_attention=False)
 
   config_proto = utils.get_config_proto()
   train_sess = tf.Session(config=config_proto, graph=train_model.graph)
   eval_sess = tf.Session(config=config_proto, graph=eval_model.graph)
-  #infer_sess = tf.Session(config=config_proto, graph=infer_model.graph)
+  infer_sess = tf.Session(config=config_proto, graph=infer_model.graph)
 
-  #sample_data = utils.load_data(args.src_dev_dir)
+  sample_data = utils.load_data(args.infer_dir)
 
   with train_model.graph.as_default():
     loaded_train_model, global_step = model_helper.create_or_load_model(
@@ -85,16 +87,16 @@ def main(args):
             (global_step, train_loss, train_rec_loss, train_kl_loss, train_rec_ppl, \
               time.time() - start_time)
         start_time = time.time()
-      # if global_step % args.eval_step == 0:
-      #   loaded_train_model.saver.save(train_sess,
-      #       os.path.join(args.save_dir, "gen_review.ckpt"), global_step=global_step)
-      #   eval_start_time = time.time()
+      if global_step % args.eval_step == 0:
+        loaded_train_model.saver.save(train_sess,
+            os.path.join(args.save_dir, "vrae.ckpt"), global_step=global_step)
+      #  eval_start_time = time.time()
       #   eval_avg_loss, eval_ppl = run_internal_eval(
       #       args, eval_model, eval_sess, args.save_dir, eval_data_vec)
       #   print "eval loss: %f, eval ppl: %.2f after training of step %d, time %.2fs" % \
       #       (eval_avg_loss, eval_ppl, global_step, time.time() - eval_start_time)
-      #   run_sample_decode(args, infer_model, infer_sess, args.save_dir, infer_data_vec)
-      #   step_start_time = time.time()
+        run_sample_decode(args, infer_model, infer_sess, args.save_dir, sample_data)
+        step_start_time = time.time()
       # if args.anneal and global_step > (train_data_size / batch_size) * args.anneal_start:
       #   train_sess.run(train_model.learning_rate_decay_op)
     except tf.errors.OutOfRangeError:
